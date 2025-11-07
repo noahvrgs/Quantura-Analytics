e7ff0bf2e6d57b4f722787d62974211f6f0a1994
Noah Vargas <113941682+noahvrgs@users.noreply.github.com>
2025-11-07 15:51:54 -0800

    Initialize project with empty folders

---
+++ quantara/backend/src/main/java/com/quantara/QuantaraApplication.java
@@ -0,0 +1,13 @@
+package com.quantara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantaraApplication.class, args);
+	}
+
+}
---
+++ quantara/backend/src/test/java/com/quantara/QuantaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
